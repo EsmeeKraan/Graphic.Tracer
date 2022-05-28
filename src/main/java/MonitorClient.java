858c97afea7d50a17ca267a995c8257b9c9ab437
Esmee <esmeekraan2@gmail.com>
2022-05-28 15:45:39 +0200

    Jar3.0 voor het testen van de monitoring op de servers.

--- src/main/java/MonitorClient.java
+++ src/main/java/MonitorClient.java
@@ -15,7 +15,7 @@ public class MonitorClient {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Verbinden met: 192.168.10.21");
-        Socket socket = new Socket("192.168.10.21", 6969);
+        Socket socket = new Socket(args[1], Integer.parseInt(args[2]));
         var output = socket.getOutputStream();
         OperatingSystemMXBean osBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
         while (true) {
